e7a64470d63183f095a7b2c62a1be523786f6873
k.pettes <k.pettes@bw-lp>
2016-10-31 15:39:40 +0100

    Forgot this one.

--- 04_objektorientierung/08_interfaces/src/tpe/oo/interfaces/impl/AnalogControllerImp.java
+++ 04_objektorientierung/08_interfaces/src/tpe/oo/interfaces/impl/AnalogControllerImp.java
@@ -11,37 +11,41 @@ import tpe.oo.interfaces.api.AnalogController;
  */
 public class AnalogControllerImp implements AnalogController {
 
-    private double x = 0.0;
-    private double y = 0.0;
+    private double xPosition = 0.0; // Membervariable der x Position.
+    private double yPosition = 0.0; // Membervariable der y Position.
 
     /**
      * @see tpe.oo.interfaces.api.AnalogController#up
      */
+    @Override
     public void up(double percentage) {
-        y -= percentage;
+        yPosition -= percentage;
     }
 
     /**
      * @see tpe.oo.interface.api.AnalogController#down
      */
+    @Override
     public void down(double percentage) {
-        y += percentage;
+        yPosition += percentage;
     }
 
     /**
      * @see tpe.oo.interface.api.AnalogController#left
      */
+    @Override
     public void left(double percentage) {
-        x -= percentage;
+        xPosition -= percentage;
     }
 
     /**
      * @see tpe.oo.interface.api.AnalgoController#right
      */
+    @Override
     public void right(double percentage) {
-        x += percentage;
+        xPosition += percentage;
     }
     public Point getPosition() {
-        return new Point((int)x,(int)y);
+        return new Point((int)xPosition,(int)yPosition);
     }
 }
