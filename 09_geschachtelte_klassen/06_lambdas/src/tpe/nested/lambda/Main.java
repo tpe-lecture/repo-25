ded5254b71211a0b526c5fb641d11a91126731d7
k.pettes <k.pettes@bw-lp>
2016-12-05 12:09:53 +0100

    Lambdas. 12-5-16

--- 09_geschachtelte_klassen/06_lambdas/src/tpe/nested/lambda/Main.java
+++ 09_geschachtelte_klassen/06_lambdas/src/tpe/nested/lambda/Main.java
@@ -7,9 +7,8 @@ import java.util.Arrays;
  */
 public class Main {
 
-    private static final int[] ZAHLEN = {
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
-    };
+    private static final int[] ZAHLEN = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+            13, 14, 15, 16, 17, 18, 19, 20 };
 
     /**
      * Hauptmethode.
@@ -21,13 +20,11 @@ public class Main {
         NumberSelector s = new NumberSelector();
 
         // TODO: Gerade Zaheln filtern
-        int[] gerade = null;
-
+        int[] gerade = s.filter((i) -> i % 2 == 0, ZAHLEN);
         System.out.println(Arrays.toString(gerade));
 
         // TODO: Ungerade Zaheln filtern
-        int[] ungerade = null;
-
+        int[] ungerade = s.filter((i) -> i % 2 == 1, ZAHLEN);
         System.out.println(Arrays.toString(ungerade));
 
     }
